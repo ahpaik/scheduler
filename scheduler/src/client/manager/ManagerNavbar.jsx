184748d84bbb07280a1dd9b3ddf99f59aadd4f94
Ally Paik <ahpaik@wpi.edu>
2026-04-27 12:44:08 -0400

    Added custom colors and font, applied to navbars

--- scheduler/src/client/manager/ManagerNavbar.jsx
+++ scheduler/src/client/manager/ManagerNavbar.jsx
@@ -9,7 +9,7 @@ const ManagerNavbar = () => {
     return (
         <nav className="navbar w-full mt-5 flex flex-col gap-y-2 px-10">
             <div className="flex items-center justify-between w-full mb-2">
-                <Link to="/manager_home" className="text-2xl font-bold tracking-tight hover:text-gray-600">
+                <Link to="/manager_home" className="text-2xl font-bold tracking-tight text-blue-dark hover:text-blue-dark-hover duration-200">
                     Scheduler
                 </Link>
                 <ul className="nav-links flex items-center list-none gap-x-8">
@@ -21,7 +21,7 @@ const ManagerNavbar = () => {
             <div className="flex flex-start">
                 {showBack && (
                     <button onClick={() => navigate(-1)} className="back-button">
-                        <span className="font-bold cursor-pointer bg-amber-400 hover:bg-amber-500 text-black py-1 px-4 rounded mt-4">&larr;</span>
+                        <span className="font-bold cursor-pointer bg-blue-light hover:bg-blue-light-hover text-black py-1 px-4 rounded mt-4">&larr;</span>
                     </button>
                 )}
             </div>
